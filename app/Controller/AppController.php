a445c3f7aa0b71c65530b89ae995c46bde241f5b
Brian Adams <wreality@gmail.com>
2014-02-21 22:35:10 -0500

    Remove internal IP custom filter

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -48,11 +48,6 @@ class AppController extends Controller {
  */
 	public function beforeFilter() {
 		$this->Setting->getSettings();
-		
-		$this->request->addDetector('internalIp', array(
-			'env' => 'REMOTE_ADDR',
-			'pattern' => '/^(192\.168\.0\.|192\.168\.1\.|127\.0\.0\.1|::1)/',
-		));
 		parent::beforeFilter();
 	}
 
